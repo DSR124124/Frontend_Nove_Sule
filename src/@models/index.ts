8cc8b0e0efa5a3c840e92977359af887d977de8c
Diego Denilson Sullcaray Ramos <u20211c348@upc.edu.pe>
2025-10-15 12:59:13 -0500

    actulizado

--- src/@models/index.ts
+++
@@ -1,8 +0,0 @@
-// Sidebar Models
-export * from '../app/layout/models/sidebar-item.model';
-
-// Auth Models
-export * from './auth/login-request.model';
-export * from './auth/login-response.model';
-
-// Add other model exports as needed
